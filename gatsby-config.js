67b93693cd404602bc87c52ce54ea679ff2f4981
Javed Akhtar <akhtar.ja@gmail.com>
2020-09-16 23:42:48 -0700

    add deployment script

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         path: path.join(__dirname, `src`, `images`),
       },
     },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: process.env.WOODWORK_JAVED_DEV_BUCKET_NAME,
+      },
+    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-theme-gallery`,
