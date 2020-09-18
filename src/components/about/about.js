8db21189dd6d7e79ff8700f38d1c7ac78ab8ad6c
Javed Akhtar <akhtar.ja@gmail.com>
2020-09-17 18:34:27 -0700

    fix typo

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,7 +6,7 @@ const About = () => {
       <p>
         I am a furniture designer and woodworker based in Vancouver, British
         Columbia, Canada. My work is modern but grounded in centuries of
-        woodworking traditions and hand-tool techniques. It is built to live
+        woodworking traditions and hand tool techniques. It is built to live
         harmoniously alongside a wide range of furniture styles, and last many
         lifetimes of daily use.
       </p>
