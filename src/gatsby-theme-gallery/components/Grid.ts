d2e80233aa32ca3a5cf82ba2309ca3d07c863b69
Javed Akhtar <akhtar.ja@gmail.com>
2020-09-17 22:40:43 -0700

    fix image tile sizes in mobile view

--- src/gatsby-theme-gallery/components/Grid.ts
+++ src/gatsby-theme-gallery/components/Grid.ts
@@ -3,7 +3,7 @@ import styled from "@emotion/styled"
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
-  grid-auto-rows: 16rem;
+  grid-auto-rows: auto;
   grid-gap: 0.5rem;
 
   @media (min-width: 1280px) {
