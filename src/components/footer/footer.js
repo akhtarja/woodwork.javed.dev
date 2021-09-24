abe92f845bfca9f5b8cb170c5a773d521baa09f1
Javed Akhtar <akhtar.ja@gmail.com>
2021-09-24 14:33:49 -0700

    update copyright year

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => (
   <footer>
-    <div>© Javed Akhtar 2020. All Rights Reserved.</div>
+    <div>© Javed Akhtar 2021. All Rights Reserved.</div>
     <div>
       Powered by{" "}
       <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
